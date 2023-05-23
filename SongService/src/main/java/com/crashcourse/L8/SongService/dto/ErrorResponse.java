4fcbdf37f8b26a0b52e6b9994569c6fa95353ad2
snuffysam <bookboy42@gmail.com>
2023-05-23 12:08:12 -0400

    Uploaded auto-generated files

--- SongService/src/main/java/com/crashcourse/L8/SongService/dto/ErrorResponse.java
+++
@@ -1,8 +0,0 @@
-package com.crashcourse.L8.SongService.dto;
-
-public class ErrorResponse {
-    private String message;
-    public ErrorResponse(String message) {
-        this.message = message;
-    }
-}
